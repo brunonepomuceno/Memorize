ee256a1c6a48907a1b17df5193aae33f1806df90
Bruno Nepomuceno <brunobezerranepomuceno@gmail.com>
2021-11-29 20:08:43 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bruno Nepomuceno on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
