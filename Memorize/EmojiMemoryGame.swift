e469ef9676c1c6bfc2a13dc29d5f783bb98d5d89
Bruno Nepomuceno <brunobezerranepomuceno@gmail.com>
2021-12-05 18:22:06 -0300

    Animation

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -11,7 +11,7 @@ class EmojiMemoryGame: ObservableObject {
     @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        let emojis = ["🧙🏽‍♂️","🎅🏽","🥷🏽"]
+        let emojis = ["🧙🏽‍♂️","🎅🏽","🥷🏽","🧛🏽","🧟‍♂️","🧞‍♂️"]
         return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
             return emojis[pairIndex]
         }
@@ -27,5 +27,9 @@ class EmojiMemoryGame: ObservableObject {
     
     func choose(card: MemoryGame<String>.Card) {
         model.choose(card: card)
+    }
+    
+    func resetGame() {
+        model = EmojiMemoryGame.createMemoryGame()
     }
 }
